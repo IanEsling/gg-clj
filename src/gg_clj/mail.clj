c8689a7709e089395afa2e332f703d28fa9e1d1d
Ian <ian.esling@gmail.com>
2012-11-20 15:36:42 +0000

    emailable races

--- src/gg_clj/mail.clj
+++ src/gg_clj/mail.clj
@@ -1,9 +1,39 @@
 (ns gg-clj.mail
-  (:use [clojure.string :exclude [reverse replace]]))
+  (:use [clojure.string :exclude [reverse]]))
 
-(defn emailable-race [race]
-    (assoc race :bettable (< 0 (count
-							(filter #(let [	components (for [s (split % #"/")] (read-string s))
+(defn add-bettable [race]
+      (assoc race :bettable (< 0 (count
+									(filter #(let [	components (for [s (split (replace % "Evs" "1/1") #"/")] (read-string s))
         									odds (/ (first components) (second components))]
 										(and (>= 2 odds) (<= 1 odds)))
-							(map #(:odds %) (:horses race)))))))
+									(map #(:odds %) (:horses race)))))))
+
+(defn sorted-odds [race]
+  (sort
+  	(map #(let [components (for [s (split (replace % "Evs" "1/1") #"/")] (read-string s))]
+        	(/ (first components) (second components)))
+   (map #(:odds %) (:horses race)))))
+
+(defn add-difference-in-odds [race]
+  (let [odds (sorted-odds race)
+        first (first odds)
+        second (second odds)]
+  	(assoc race :odds-diff (- second first))))
+
+(defn magic-number [horse race]
+    	(- (+ (:odds-diff race)
+           (:tips horse))
+           (:runners race)))
+
+(defn add-magic-number [race]
+  (assoc race :horses
+	(for [horse (:horses race)]
+		(assoc horse :magic-number (magic-number horse race)))))
+
+(defn emailable-race [race]
+	(-> (add-bettable race) (add-difference-in-odds) (add-magic-number)))
+
+(defn emailable-races [races]
+  (filter #(:bettable %)
+          (for [race races]
+            (emailable-race race))))
--- test/gg_clj/mail_test.clj
+++ test/gg_clj/mail_test.clj
@@ -2,18 +2,45 @@
   (:use gg-clj.mail)
   (:use midje.sweet))
 
-(def bettable-race {:venue "V" 
-                    :time "12.34" 
-                    :runners 3
-                    :horses '({:name "h1"
-                               :odds "2/1"
-                               :tips 2
-                               }
-                              {:name "h2"
-                               :odds "3/1"
-                               :tips 1
-                               })})
+(def race {:venue "V" :time "12.34" :runners 4
+                    :horses '({:name "h1" :odds "2/1" :tips 2}
+                              {:name "h2" :odds "3/1" :tips 1})})
 
+(def race2 {:venue "V" :time "13.34" :runners 11
+                    :horses '({:name "h3" :odds "3/1" :tips 4}
+                              {:name "h4" :odds "Evs" :tips 1})})
+
+(def race3 {:venue "V" :time "14.34" :runners 9
+                    :horses '({:name "h5" :odds "3/1" :tips 4}
+                              {:name "h6" :odds "9/10" :tips 1})})
+
+
+(fact "only bettable races should be emailed"
+      (count (emailable-races [race race2 race3])) => 2
+      (map #(:time %) (emailable-races [race race2 race3])) => (contains ["12.34" "13.34"] :in-any-order)
+      )
+
+
+(fact "horses should have magic numbers calculated"
+      	(every? #(:magic-number %) (:horses (emailable-race race))) => true
+		(:magic-number (first (filter #(= (:name %) "h1") (:horses (emailable-race race))))) => -1
+      	(:magic-number (first (filter #(= (:name %) "h4") (:horses (emailable-race race2))))) => -8
+      )
 
 (fact "should calculate if race is bettable"
-	(emailable-race bettable-race) => (contains {:bettable true}))
+	(emailable-race race) => (contains {:bettable true})
+	(emailable-race (assoc race :horses (for [horse (:horses race)] 
+											(if (= (:name horse) "h1")
+                                          		(assoc horse :odds "3/1")
+                                              	horse)))) 
+      => (contains {:bettable false})
+	(emailable-race (assoc race :horses (for [horse (:horses race)] 
+											(if (= (:name horse) "h1")
+                                          		(assoc horse :odds "Evs")
+                                              horse))))
+      => (contains {:bettable true})
+	(emailable-race (assoc race :horses (for [horse (:horses race)] 
+											(if (= (:name horse) "h1")
+                                          		(assoc horse :odds "99/100")
+                                              horse))))
+      => (contains {:bettable false}))
