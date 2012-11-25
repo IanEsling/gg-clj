5aae22e8a2ecca76a42991a77090c455b32388ec
Ian <ian.esling@gmail.com>
2012-11-25 23:39:39 +0000

    put Paul and Dad on the email list

--- src/gg_clj/mail.clj
+++ src/gg_clj/mail.clj
@@ -120,8 +120,8 @@
                   :user (System/getenv "SENDGRID_USERNAME")
                   :pass (System/getenv "SENDGRID_PASSWORD")}
                 {:from "geegees@geegees.com"
-                 ;;:to ["ian.esling@gmail.com" "pesling@gmail.com" "aliciales@esling.me.uk"]
-                 :to "ian.esling@gmail.com"
+                 :to ["ian.esling@gmail.com" "pesling@gmail.com" "aliciales@esling.me.uk"]
+                 ;;:to "ian.esling@gmail.com"
                  :subject "Today's GeeGees Tips"
                  :body [{:type "text/html"
                          :content (races-html (emailable-races races))}]}))
