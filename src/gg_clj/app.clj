bf7cc3ed06b5b777be459f59aa2b0b3cd6d6eb27
Ian <ian.esling@gmail.com>
2012-12-05 00:40:16 +0000

    return lay bet html from app

--- src/gg_clj/app.clj
+++ src/gg_clj/app.clj
@@ -1,9 +1,15 @@
 (ns gg-clj.app
-   (:use [compojure.core :only [defroutes GET]])
+  (:use [gg-clj.db :as db])
+  (:use [gg-clj.mail :as mail])
+  (:use [compojure.core :only [defroutes GET]])
   (:require [ring.adapter.jetty :as ring]))
 
 (defroutes routes
-  (GET "/" [] "<h2>Hello World</h2>"))
+  (GET "/"
+       []
+       (if (db/race-day-today-exists)
+         (mail/lay-races-html (:races (db/get-race-day)) "Today's Lay Bets")
+         (mail/lay-races-html (:races (db/get-latest-race-day)) "Latest Lay Bets"))))
 
 (defn start []
   (ring/run-jetty #'routes {:port 8080 :join? false}))
